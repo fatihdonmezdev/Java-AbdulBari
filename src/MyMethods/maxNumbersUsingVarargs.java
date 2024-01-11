f60bdba3e421ff78c2ebf41bbbb0efa8fbd6e72e
Fatih <fdonmez_1512@hotmail.com>
2024-01-11 23:46:05 +0300

    Added max num calculator.

--- src/MyMethods/maxNumbersUsingVarargs.java
+++ src/MyMethods/maxNumbersUsingVarargs.java
@@ -6,11 +6,10 @@ public class maxNumbersUsingVarargs {
         int maxvalue = 0;
         if(A.length == 0 || A.length == 1){
             System.out.println("No or 1 argument(s) given!");
-        }else{
-            for(int i = 1; i < A.length; i++){
-                if(A[i]>maxvalue){
-                    maxvalue = A[i];
-                }
+        }
+        for(int i = 1; i < A.length; i++){
+            if(A[i]>maxvalue){
+                maxvalue = A[i];
             }
         }
         return maxvalue;
