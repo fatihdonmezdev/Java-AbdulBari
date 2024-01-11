89bbf7c4faa0c5a06456a23575848e0180c7a50b
Fatih <fdonmez_1512@hotmail.com>
2024-01-11 23:41:46 +0300

    Added max num calculator.

---
+++ src/MyMethods/calculateDiscountVarArguments.java
@@ -0,0 +1,4 @@
+package MyMethods;
+
+public class calculateDiscountVarArguments {
+}
---
+++ src/MyMethods/maxNumbersUsingVarargs.java
@@ -0,0 +1,23 @@
+package MyMethods;
+
+public class maxNumbersUsingVarargs {
+
+    static int maxCalculator(int... A){
+        int maxvalue = 0;
+        if(A.length == 0 || A.length == 1){
+            System.out.println("No or 1 argument(s) given!");
+        }else{
+            for(int i = 1; i < A.length; i++){
+                if(A[i]>maxvalue){
+                    maxvalue = A[i];
+                }
+            }
+        }
+        return maxvalue;
+    }
+    public static void main(String[] args) {
+        int val = maxCalculator(1,2,333,422,66,89);
+        System.out.println(val);
+    }
+
+    }
