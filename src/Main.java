753b6b8413b89fe640214f9a2e1c9534c64edb0f
Fatih <fdonmez_1512@hotmail.com>
2024-01-11 16:17:15 +0300

    I'll upload Udemy course student assignments

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+
+    static boolean isPrime(int x){
+        double halfOfTheGivenNumber = (x / 2.0);
+
+                for(int i=2; i <= halfOfTheGivenNumber ; i++){
+                    if(x % i == 0){
+                        return false;
+                    }
+        }
+        return true;
+
+    };
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a number;");
+
+        int sayi = input.nextInt();
+        boolean result = isPrime(sayi);
+        if(result){
+            System.out.println("This number IS a prime number!");
+        }else{
+            System.out.println("This number is NOT a prime number");
+        }
+
+
+    }
+}
